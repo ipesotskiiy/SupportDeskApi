42583ccf57b0ea4f6c0ea1264ed143ab88b3754f
ipesotskiiy <pesotskygoscha@yandex.ru>
2026-06-23 13:07:46 +0300

    feat: added filters

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -19,12 +21,56 @@ class TicketCategoryViewSet(ModelViewSet):
     queryset = TicketCategory.objects.all()
     serializer_class = TicketCategorySerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+    filterset_fields = ("name", "slug")
+    search_fields = (
+        "name",
+        "slug",
+        "description",
+    )
+    ordering_fields = (
+        "id",
+        "name",
+        "created_at",
+        "updated_at",
+    )
+    ordering = ("name",)
 
 
 class TicketViewSet(ModelViewSet):
     queryset = Ticket.objects.select_related("author", "category").all()
     serializer_class = TicketSerializer
     permission_classes = (IsAuthenticated, IsOwnerOrStaff)
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+    filterset_fields = (
+        "status",
+        "priority",
+        "category",
+    )
+    search_fields = (
+        "title",
+        "description",
+        "author__username",
+        "category__name",
+    )
+    ordering_fields = (
+        "id",
+        "title",
+        "status",
+        "priority",
+        "created_at",
+        "updated_at",
+        "closed_at",
+    )
+    ordering = ("-created_at",)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -42,6 +88,23 @@ class TicketCommentViewSet(ModelViewSet):
     queryset = TicketComment.objects.select_related("author", "ticket").all()
     serializer_class = TicketCommentSerializer
     permission_classes = (IsAuthenticated, IsOwnerOrStaff)
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+    filterset_fields = ("ticket",)
+    search_fields = (
+        "text",
+        "author__username",
+        "ticket__title",
+    )
+    ordering_fields = (
+        "id",
+        "created_at",
+        "updated_at",
+    )
+    ordering = ("created_at",)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
