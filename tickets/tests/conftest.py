abcf0eea6309bb4d181980203a7c970a7a2cec6a
ipesotskiiy <pesotskygoscha@yandex.ru>
2026-06-23 17:48:21 +0300

    feat: added business rules and permissions tests

--- tickets/tests/conftest.py
+++ tickets/tests/conftest.py
@@ -1,8 +1,9 @@
 import pytest
+from django.utils import timezone
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
 
-from tickets.models import TicketCategory
+from tickets.models import TicketCategory, Ticket, TicketStatus
 
 pytestmark = pytest.mark.django_db
 
@@ -20,6 +21,17 @@ def user():
         password="fusiontech",
     )
     return user_obj
+
+
+@pytest.fixture
+def second_user():
+    user_model = get_user_model()
+    second_user_obj = user_model.objects.create_user(
+        username="second_no_staff_user",
+        email="second_test@mail.com",
+        password="fusiontech2",
+    )
+    return second_user_obj
 
 
 @pytest.fixture
@@ -41,6 +53,12 @@ def authenticated_client(api_client, user):
 
 
 @pytest.fixture
+def second_authenticated_client(api_client, second_user):
+    api_client.force_authenticate(user=second_user)
+    return api_client
+
+
+@pytest.fixture
 def staff_authenticated_client(api_client, staff_user):
     api_client.force_authenticate(user=staff_user)
     return api_client
@@ -52,4 +70,39 @@ def ticket_category():
         name="Authentication",
         slug="authentication",
         description="Login, password and account access problems",
+    )
+
+
+@pytest.fixture
+def user_ticket(user, ticket_category):
+    return Ticket.objects.create(
+        author=user,
+        category=ticket_category,
+        title="Cannot login 2",
+        description="User cannot login after password reset",
+        priority="high",
+    )
+
+
+@pytest.fixture
+def second_user_ticket(second_user, ticket_category):
+    return Ticket.objects.create(
+        author=second_user,
+        category=ticket_category,
+        title="Cannot login 3",
+        description="Second user cannot login after password reset",
+        priority="high",
+    )
+
+
+@pytest.fixture
+def closed_user_ticket(user, ticket_category):
+    return Ticket.objects.create(
+        author=user,
+        category=ticket_category,
+        title="Cannot login 4",
+        description="Closed user cannot login after password reset",
+        priority="high",
+        status=TicketStatus.CLOSED,
+        closed_at=timezone.now()
     )
